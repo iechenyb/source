c19c6e603f4a7ca09dd90c52596ef8c3dde318f7
iechenyb <zzuchenyb@sina.com>
2016-12-24 17:02:24 +0800

    dsdfsd

--- page/moban2/moban.js
+++ page/moban2/moban.js
@@ -13,13 +13,6 @@ app.directive("directive", function() {
         transclude:true
     };
 });
-app.directive("header", function() {
-    return {
-        restrict : "E",
-        template : "header content!",
-        transclude:true
-    };
-});
 app.directive('expander', function() {
     return {
         restrict : 'EA',
@@ -40,7 +33,75 @@ app.directive('expander', function() {
         }
     }
 });
+app.directive('accordion', function() {
+    return {
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        template : '<div ng-transclude></div>',
+        controller : function() {
+            var expanders = [];
+            this.gotOpened = function(selectedExpander) {
+                angular.forEach(expanders, function(expander) {
+                    if (selectedExpander != expander) {
+                        expander.showMe = false;
+                    }
+                });
+            }
+            this.addExpander = function(expander) {
+                expanders.push(expander);
+            }
+        }
+    }
+});
+app.directive('expander1', function() {
+    return {
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        require : '^?accordion',
+        scope : {
+            title : '=expanderTitle'
+        },
+        template : '<div>'
+        + '<div class="title" ng-click="toggle()">{{title}}</div>'
+        + '<div class="body" ng-show="showMe" ng-transclude></div>'
+        + '</div>',
+        link : function(scope, element, attrs, accordionController) {
+            scope.showMe = false;
+            accordionController.addExpander(scope);
+            scope.toggle = function toggle() {
+                scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
+            }
+        }
+    }
+});
+
+app.directive('ybheader', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'iner.html',
+        bindings:{links:'='},
+        controllerAs:"ybheader",
+        controller:function($scope,$rootScope,$state){
+            console.log("ybheader !");
+            $scope.name="chneyuanbao";
+            $rootScope.name="xxhsl";
+        }
+    };
+});
 app.controller('mycontroller',function($scope) {
     $scope.title = '点击展开';
     $scope.text = '这里是内部的内容。';
+    $scope.expanders = [{
+        title : 'Click me to expand',
+        text : 'Hi there folks, I am the content that was hidden but is now shown.'
+    }, {
+        title : 'Click this',
+        text : 'I am even better text than you have seen previously'
+    }, {
+        title : 'Test',
+        text : 'haha'
+    }];
 });
